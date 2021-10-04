a23efeff50bb78a5bd844ebcf0a1a45ef04765f6
Italo Igor <italo013@hotmail.com>
2021-10-04 16:25:52 -0300

    Delete lixo.py

--- lixo.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import os
-from pathlib import Path
-
-
-#lx_morta = pd.read_excel(str(Path(os.getcwd())) + '\\Tábuas\\Funções Biométricas (Com Fórmulas).xlsx', engine='openpyxl')#, usecols=[tabua_mortalidade])
-print(Path(os.getcwd()))
